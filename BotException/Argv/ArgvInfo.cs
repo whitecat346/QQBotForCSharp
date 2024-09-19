e5710b497ebe0ae921ebc9ab38e95b3716929002
WhiteCAT <yanyuhang080901qw@outlook.com>
2024-09-19 23:38:42 +0800

    创建 MakeQuate 方法

--- BotException/Argv/ArgvInfo.cs
+++ BotException/Argv/ArgvInfo.cs
@@ -6,16 +6,28 @@ using System.Threading.Tasks;
 
 namespace QQBotForCSharp.BotException.Argv
 {
+    #region FunctionArgvInfo
+
     public struct FunctionArgvInfo
     {
         public int Count { get; set; }
-        public string[] Args { get; set; }
     }
+
+    #endregion
 
     public class ArgvInfo
     {
+        public static readonly Dictionary<string, FunctionArgvInfo> FunctionArgvInfosDictionary = new()
+        {
+            { "#echo", new FunctionArgvInfo() { Count = int.MaxValue } },
+            { "#randImg", new FunctionArgvInfo() { Count = 0 } },
+            { "#netRandImg", new FunctionArgvInfo() { Count = 0 } },
+            { "#ban", new FunctionArgvInfo() { Count = 2 } },
+            { "#unban", new FunctionArgvInfo() { Count = 1 } },
+            { "#help", new FunctionArgvInfo() { Count = 1 } },
+            { "#stopBot", new FunctionArgvInfo() { Count = 0 } },
+        };
 
-
-        public Dictionary<string, FunctionArgvInfo> FunctionArgvInfos { get; set; }
+        public Dictionary<string, FunctionArgvInfo> FunctionArgvInfos => FunctionArgvInfosDictionary;
     }
 }
--- BotException/ArgvCheck.cs
+++ BotException/ArgvCheck.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using QQBotForCSharp.BotException.Argv;
 
 namespace QQBotForCSharp.BotException
 {
@@ -14,17 +16,51 @@ namespace QQBotForCSharp.BotException
             NeedMoreArgvs
         }
 
-        public static string MakeExceptionInfo( string [ ] argv, ExceptionType type, int begin, int end = 0, int ptrPos = 0 )
+        public static string MakeQuate( ref string [ ] argv, int begin, int end = 0, int ptrPos = 0 )
         {
+            var spaceCount = 0;
+            for (int i = 0; i < begin; i++)
+                spaceCount += argv[i].Length + 1;
 
+            string spaceStr = new(' ', spaceCount);
+            if (end == 0)
+            {
+                var sumLength = argv.Sum( s => s.Length + 1 );
+                spaceStr += new string( '~', sumLength - spaceCount - 1 ).Insert( ptrPos, "^" );
+            }
+            else
+            {
+                var sumLength = 0;
+                for (int i = begin; i < end; i++)
+                    sumLength += argv[i].Length + 1;
+                sumLength--;
+
+                spaceStr += new string('~', sumLength).Insert(ptrPos, "^");
+            }
+
+            return spaceStr;
+        }
+
+        public static string MakeExceptionInfo( ref string [ ] argv, ExceptionType type, int begin, int end = 0, int ptrPos = 0 )
+        {
+            return MakeQuate( ref argv, begin, end, ptrPos );
         }
     }
 
     public class ArgvCheck
     {
-        public static void CheckArgv( string [ ] args )
+        public static string CheckArgv( string [ ] args )
         {
-
+            ArgvInfo.FunctionArgvInfosDictionary.TryGetValue( args [0], out var value );
+            if (value.Count > args.Length - 1)
+            {
+                return ArgvExceptions.MakeExceptionInfo( ref args, ArgvExceptions.ExceptionType.TooManyArgvs, 1 );
+            }
+            else if (value.Count < args.Length - 1)
+            {
+                return ArgvExceptions.MakeExceptionInfo( ref args, ArgvExceptions.ExceptionType.NeedMoreArgvs, 1 );
+            }
+            else return string.Empty;
         }
     }
 }
